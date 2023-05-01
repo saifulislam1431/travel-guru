7bc624457049e150ecbd33bfcfaa34608b587cde
Saiful Islam <sakibbhaiya143@gmail.com>
2023-05-01 12:12:40 +0600

    Initial commit and project setup

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+        
+"primary": "#f3bafc",
+        
+"secondary": "#f7761b",
+        
+"accent": "#ed97ea",
+        
+"neutral": "#262027",
+        
+"base-100": "#E4E7EC",
+        
+"info": "#327DCD",
+        
+"success": "#6AE7A8",
+        
+"warning": "#F5BF42",
+        
+"error": "#EE2F6F",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+}
+
