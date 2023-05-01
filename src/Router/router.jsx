cd173391605b4cbf4dd0fc477f011b6852919439
Saiful Islam <sakibbhaiya143@gmail.com>
2023-05-01 13:58:54 +0600

    Router age page setup

--- src/Router/router.jsx
+++ src/Router/router.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
+import Main from '../Layouts/Main';
+import Home from '../Pages/Home/Home';
+import Error from '../Pages/Error/Error';
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <div>Hello</div>
+        element: <Main></Main>,
+        errorElement : <Error></Error>,
+        children:[
+            {
+                path: "/",
+                element: <Home></Home>
+            }
+        ]
     }
 ])
 export default router;
